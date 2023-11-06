50047aabc099aad90e58c8663f5c9ff669520553
Jake Hessian <jake.hessian@gmail.com>
2023-11-05 20:50:41 -0500

    Updated for games in 2024 & games that end on next day @12am

--- main_scraper.py
+++ main_scraper.py
@@ -5,70 +5,71 @@ from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
 from ics import Calendar, Event
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from pytz import timezone
 
-local_tz = timezone('America/New_York')  # Adjusted to my local timezone for ics file
+local_tz = timezone('America/New_York') 
 
-# Set up the driver (site is javascript generated)
 options = webdriver.ChromeOptions()
 options.headless = True
 driver = webdriver.Chrome(options=options)
 
-# Navigate to the hockey schedule webpage
-url = "https://anc.ca.apm.activecommunities.com/activewaterloo/leagues/info?onlineSiteId=0&league_id=dlpwb0prbWVOTzJXVGVOSGFKUG5Fdz09&display_index=3"
-driver.get(url)
+try:
+    driver.get("https://anc.ca.apm.activecommunities.com/activewaterloo/leagues/info?league_id=THZYZUF2SWVQYUY4ZHhPUE5MSi8rdz09&display_index=1")
+    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, 'league-schedule-card')))
+    html_content = driver.page_source
+except Exception as e:
+    print("An error occurred:", e)
+finally:
+    driver.quit()
 
-# Wait for the content to load 10 seconds
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'league-schedule-card')))
-
-# Get page source
-html_content = driver.page_source
-
-# Close the browser window
-driver.quit()
-
-# Parse the HTML content using BeautifulSoup
 soup = BeautifulSoup(html_content, 'html.parser')
-
-# Find all the game segments
 games = soup.find_all('div', class_='league-schedule-card')
 
-# Loop through each game and extract information
-for game in games:
-    home_team = game.find('div', class_='schedule-card__home-wrapper').find('a').text.strip()
-    away_team = game.find('div', class_='schedule-card__away-wrapper').find('a').text.strip()
-    date = game.find('div', class_='date').text.strip()
-    game_time = game.find('div', class_='time-and-status').span.text.strip()
-    location = game.find('a', class_='facility-name').text.strip()
-
-# Create a new calendar
 cal = Calendar()
 
-# Loop through each game and extract information
+current_date = datetime.now(local_tz)
+current_year = current_date.year
+
 for game in games:
     home_team = game.find('div', class_='schedule-card__home-wrapper').find('a').text.strip()
     away_team = game.find('div', class_='schedule-card__away-wrapper').find('a').text.strip()
-
-    # Only add games for my team "INDY #4"
 
     if home_team == "Indy #4" or away_team == "Indy #4":
         date = game.find('div', class_='date').text.strip()
         game_time = game.find('div', class_='time-and-status').span.text.strip()
         location = game.find('a', class_='facility-name').text.strip()
 
-        # Convert date and time to a datetime object with timezone
-        start_time_str = f"{date} {game_time.split(' - ')[0]} 2023"  # Assuming the year is 2023 update later for full sched
-        start_time = local_tz.localize(datetime.strptime(start_time_str, '%d %b %I:%M %p %Y'))
-        end_time_str = f"{date} {game_time.split(' - ')[1]} 2023"
-        end_time = local_tz.localize(datetime.strptime(end_time_str, '%d %b %I:%M %p %Y'))
+        # Parse the date and time into a datetime object
+        game_month_day = datetime.strptime(date, '%d %b').replace(year=current_year)
+        start_time = datetime.strptime(game_time.split(' - ')[0], '%I:%M %p').time()
+        end_time = datetime.strptime(game_time.split(' - ')[1], '%I:%M %p').time()
 
-        # Create an event
-        event = Event(name=f"{home_team} vs {away_team}", begin=start_time, end=end_time, location=location)
+        # Combine the parsed date and time, localize to the correct timezone
+        game_start = local_tz.localize(datetime.combine(game_month_day, start_time))
+        game_end = local_tz.localize(datetime.combine(game_month_day, end_time))
+
+        # Check if the game is in the next year
+        if game_start < current_date:
+            game_start = game_start.replace(year=current_year + 1)
+            game_end = game_end.replace(year=current_year + 1)
+
+        # Adjust for games that end after midnight
+        if game_end < game_start:
+            game_end += timedelta(days=1)
+
+        print('=' * 50)
+        print(f"Creating an event with the following details:")
+        print(f"Home Team: {home_team}")
+        print(f"Away Team: {away_team}")
+        print(f"Start Time: {game_start}")
+        print(f"End Time: {game_end}")
+        print(f"Location: {location}")
+
+        event = Event(name=f"{home_team} vs {away_team}", begin=game_start, end=game_end, location=location)
         cal.events.add(event)
 
-# Save the calendar to an .ics file
 with open('games.ics', 'w') as f:
     f.write(str(cal))
 
